bf962480641f0b1a6c547fa7ab2c69a7934329b5
matheus mozart <mmsnborges@gmail.com>
2024-10-12 22:29:16 -0300

    finalizado

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -24,13 +24,13 @@ const tabs = [
 const Container = () => {
     return (
       <section className="w-full min-h-[85vh] h-auto bg-darkBlue flex flex-col md:flex-row items-center justify-center">
-        <div className="w-full md:w-1/2 flex items-center justify-center p-4"> {/* Alterado para responsividade */}
-          <img 
-            src="https://via.placeholder.com/300" 
-            alt="Descrição da imagem" 
-            className="max-w-full h-auto" 
-          />
-        </div>
+       <div className="w-full md:w-1/2 flex items-center justify-center p-4"> 
+        <img 
+          src="https://placedog.net/300/400" 
+          alt="Descrição da imagem" 
+          className="max-w-full h-auto rounded-lg" 
+        />
+      </div>
 
         <div className="w-full md:w-1/2 flex items-start justify-start flex-col p-4 text-tertiary">
           <h2 className="text-4xl font-bold text-left w-full m-4">Matheus Mozart</h2>
